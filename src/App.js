0eba8219bbe6ab4d3efe0855a8ddc9db3694c504
Dixa Rai <dixarai20051@gmail.com>
2023-01-31 16:59:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Player from "./component/Player";
 
 function App() {
+
+  const [songs] = useState([
+    {
+      title:"Titaliyan",
+      artist:"Hardy Sandhu",
+      img_src:"./images/titaliya.jpg",
+      src:"./songs/Titliaan Warga.mp3",
+    },
+    {
+      title:"Care ni karda",
+      artist:"Yo Yo Honey Singh",
+      img_src:"./images/care-ni-karda.jpg",
+      src:"./songs/Care Ni Karda.mp3",
+    },
+    {
+      title:"BurjKhalifa",
+      artist:"Nikita Gandhi",
+      img_src:"./images/burjkalifa.jpg",
+      src:"./songs/BurjKhalifa.mp3",
+    },
+    {
+      title:"Nach Meri Rani",
+      artist:"Guru Randhawa",
+      img_src:"./images/nach-meri-rani.jpg",
+      src:"./songs/Naach Meri Rani.mp3",
+    }
+  ])
+
+  const [currentSongIndex, setCurrentSongIndex] = 
+  useState(0);
+  const [nextSongIndex, setNextSongIndex] = 
+  useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() =>{
+      if(currentSongIndex +1>songs.length-1){
+        return 0
+      } else{
+        return currentSongIndex+1
+      }
+        
+    })
+  },[currentSongIndex,songs.length])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player 
+        currentSongIndex = {currentSongIndex}
+        setCurrentSongIndex = {setCurrentSongIndex}
+        nextSongIndex = {nextSongIndex}
+        songs = {songs}
+      />
     </div>
   );
 }
---
+++ src/component/Details.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Details(props) {
+  return (
+    <div className='details'>
+        <div className='details_img'>
+            <img src = {props.songs.img_src} alt = ""/>
+        </div>  
+        <h3 className='details_title'>{props.songs.title}</h3> 
+        <h3 className='details_artist'>{props.songs.artist}</h3> 
+    </div>
+  )
+}
+
+export default Details
